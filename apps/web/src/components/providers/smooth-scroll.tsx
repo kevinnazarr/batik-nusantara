6bc8f3e1fa4f03c3bb67baf32dd26d8a8af17cb3
kevinnazarr <kevinnazar76@gmail.com>
2026-09-08 15:39:54 +0700

    feat: enhance SmoothScroll component with detailed options for improved performance

--- apps/web/src/components/providers/smooth-scroll.tsx
+++ apps/web/src/components/providers/smooth-scroll.tsx
@@ -9,7 +9,16 @@ interface SmoothScrollProps {
 
 export default function SmoothScroll({ children }: SmoothScrollProps) {
   return (
-    <ReactLenis root options={{ autoRaf: true, anchors: true }}>
+    <ReactLenis
+      root
+      options={{
+        autoRaf: true,
+        anchors: true,
+        duration: 1.8,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        touchMultiplier: 2,
+      }}
+    >
       {children}
     </ReactLenis>
   );
