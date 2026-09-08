b9d28e11a1814338e500f77385f7fd0c3d83969f
kevinnazarr <kevinnazar76@gmail.com>
2026-09-08 16:19:00 +0700

    feat: remove Featured component from HomePage and delete its implementation

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/page/home/hero";
-import Featured from "@/components/page/home/featured";
 
 export default function HomePage() {
   return (
     <main className="flex-1 flex flex-col">
       <Hero />
-      <Featured />
     </main>
   );
 }
